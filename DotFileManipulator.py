c43959c5dc9f35854af3d341f0251f1995d29d80
Rahul <rahulr56@users.noreply.github.com>
2017-12-29 06:05:18 -0500

    Needs a refactored dot file

--- DotFileManipulator.py
+++ DotFileManipulator.py
@@ -1,25 +1,40 @@
+#!/bin/python
+
 import re
+import os
 import sys
-import pprint 
+import pprint
+
+
 pp = pprint.PrettyPrinter(indent=4)
-f = open(sys.argv[1])	# file name
+
+
+#  Read dot file and store it as a list
+f = open(sys.argv[1])    # file name
 data = [x.strip() for x in f.readlines()]
 f.close()
 contents = " ".join(data)
+
+
+#  Identify head and tail
 head = str(contents.split('{')[0]) + '{'
-contents = " ".join(contents.split('{')[1:]) 
+contents = " ".join(contents.split('{')[1:])
 tail = '}' + str(contents.split('}')[-1])
 contents = contents.split('}')[:-1]
 contents = " ".join([x.strip() for x in contents])
-contents = contents.replace('\\\"' , '\'')
-print ('--------------------------------')
-contents = contents.split('\"')
 print (contents)
+
+#  parse each node to find if they are using system APIs
+contents = contents.replace('\\\"', '\'')
+contents = contents.split('\"')
 finalContents = ""
 for node in contents:
-	aaa = re.findall(r'(java|javax|com|org|sun|sunw|return|goto)', node) 
-	if len(aaa) > 0:
-		print ("NODE : " + node + "-------------------> " + str(len(aaa)) + "____" + str(aaa))
-	
-		finalContents += '\"' + node + '\"' +" -> "
-print(head + " " + finalContents.strip('-> ').strip()+ " " + tail)
+    aaa = re.findall(r'(java|javax|com|org|sun|sunw|return|goto|->)', node)
+    if len(aaa) > 0:
+        print ("NODE : " + node + "-------------------> " + str(len(aaa)) +
+               "____" + str(aaa))
+        finalContents += '\"' + node + '\"' + " \n "
+    else:
+        print ("--- SKIPPED : " + node + " --------> " + str(len(aaa)) +
+               "-----" + str(aaa))
+print(head + " " + finalContents.strip('-> ').strip() + " " + tail)

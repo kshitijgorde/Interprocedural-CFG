40eac71cfa7c9e1fc1271f1315eb5b6e096f05ea
rahulr56 <rahulr301@gmail.com>
2018-01-03 18:16:53 -0500

    fully qualified class path

--- ICFGCreator/ICFGCreator.sh
+++ ICFGCreator/ICFGCreator.sh
@@ -15,13 +15,16 @@ java -jar decompiler.jar "$jarFile" -o "$tempFolder"
 
 inputJarDir=$tempFolder
 files=$(grep -inr "applet" "$inputJarDir" | grep -v "abstract" | grep "extends" | head -n1)
-# fileName=$(echo "$files" | cut -d':' -f1)
-# echo "File name : $fileName"
+fileName=$(echo "$files" | cut -d':' -f1)
+echo "File name : $fileName"
 # echo "Search results: $files"
 # filePath=$(echo "$fileName" | rev | cut -d'/' -f2- | rev)
 className=$(echo "$files" | awk -F 'class' '{print $NF}' )
 className=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< "$className" | cut -d' ' -f1)
 # echo "File path : $filePath"
 # echo "Class Name : $className"
-java -jar myIcfg.jar "$className" "$jarFile.dot"
-# echo java -jar myIcfg.jar "$className" "$jarFile.dot"
+pkg=$(grep "package" $fileName | awk -F " " '{print $NF}' | cut -d';' -f1)
+# echo "Package : $pkg"
+java -jar myIcfg.jar "$pkg.$className" "$jarFile.dot" "$jarFile"
+# echo java -jar myIcfg.jar "$pkg.$className" "DotFiles/$jarFile.dot" "$jarFile"
+rm -rf $tempFolder

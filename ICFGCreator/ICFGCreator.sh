d5b7d14493ade381f9b096c03a034cbd603bf406
rahulr56 <rahulr301@gmail.com>
2018-01-03 00:01:02 -0500

    adding script to genrate icfg

---
+++ ICFGCreator/ICFGCreator.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+    echo "Usage : $0 <JAR File>"
+    exit 1
+fi
+jarFile=$1
+tempFolder="TempDecompiledJar/$1"
+
+
+#Decompile JarFile
+echo "java -jar decompiler.jar $jarFile -o $tempFolder"
+java -jar decompiler.jar "$jarFile" -o "$tempFolder"
+
+inputJarDir=$tempFolder
+files=$(grep -inr "applet" "$inputJarDir" | grep -v "abstract" | grep "extends" | head -n1)
+# fileName=$(echo "$files" | cut -d':' -f1)
+# echo "File name : $fileName"
+# echo "Search results: $files"
+# filePath=$(echo "$fileName" | rev | cut -d'/' -f2- | rev)
+className=$(echo "$files" | awk -F 'class' '{print $NF}' )
+className=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< "$className" | cut -d' ' -f1)
+# echo "File path : $filePath"
+# echo "Class Name : $className"
+java -jar myIcfg.jar "$className" "$jarFile.dot"
+# echo java -jar myIcfg.jar "$className" "$jarFile.dot"

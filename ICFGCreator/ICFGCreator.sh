007276e03fbab35267381a020e12cba101003583
rahulr56 <rahulr301@gmail.com>
2018-01-04 00:19:40 -0500

    adding working files

--- ICFGCreator/ICFGCreator.sh
+++ ICFGCreator/ICFGCreator.sh
@@ -19,7 +19,7 @@ then
     inputJarDir=$tempFolder
     files=$(grep -inr "applet" "$inputJarDir" | grep -v "abstract" | grep "extends" | head -n1)
     fileName=$(echo "$files" | cut -d':' -f1)
-    echo "File name : $fileName"
+    # echo "File name : $fileName"
     # echo "Search results: $files"
     # filePath=$(echo "$fileName" | rev | cut -d'/' -f2- | rev)
     className=$(echo "$files" | awk -F 'class' '{print $NF}' )
--- ICFGCreator/execICFGCreator.sh
+++ ICFGCreator/execICFGCreator.sh
@@ -2,15 +2,17 @@
 
 testerPath="./Tester"
 currentPath=$(pwd)
-files=$(cd $1; ls -l --color=none *.jar | awk -F " " '{print $NF}'; cd $currentPath)
+echo "Current Path : $currentPath"
+echo "PATH : $1"
+files=$(cd "$1"; ls -l --color=none *.jar | awk -F " " '{print $NF}'; cd $currentPath)
 for jarFile in $files
 do
     echo "$jarFile"
-    # echo cp "$1/$jarFile" "$testerPath"
+    echo cp "$1/$jarFile" "$testerPath"
     # echo ./ICFGCreator.sh "$testerPath/$jarFile"
     # echo rm -rf "$testerPath/$jarFile"
     # ./ICFGCreator.sh "$testerPath/$jarFile"
-	cp "$1/$jarFile" "$testerPath"
+    cp "$1/$jarFile" "$testerPath"
     jarFile=$(echo $jarFile | awk -F "/" '{print $NF}')
     ./ICFGCreator.sh "$testerPath/$jarFile"
     rm -rf $testerPath/$jarFile

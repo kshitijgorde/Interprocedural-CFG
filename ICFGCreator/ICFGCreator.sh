499619dc1523516e39c78d07e8815b7381a5b674
rahulr56 <rahulr301@gmail.com>
2018-01-04 00:07:58 -0500

    working copy

--- ICFGCreator/ICFGCreator.sh
+++ ICFGCreator/ICFGCreator.sh
@@ -8,23 +8,36 @@ fi
 jarFile=$1
 tempFolder="TempDecompiledJar/$1"
 
+echo "Working on .. $jarFile"
 
 #Decompile JarFile
-echo "java -jar decompiler.jar $jarFile -o $tempFolder"
-java -jar decompiler.jar "$jarFile" -o "$tempFolder"
-
-inputJarDir=$tempFolder
-files=$(grep -inr "applet" "$inputJarDir" | grep -v "abstract" | grep "extends" | head -n1)
-fileName=$(echo "$files" | cut -d':' -f1)
-echo "File name : $fileName"
-# echo "Search results: $files"
-# filePath=$(echo "$fileName" | rev | cut -d'/' -f2- | rev)
-className=$(echo "$files" | awk -F 'class' '{print $NF}' )
-className=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< "$className" | cut -d' ' -f1)
-# echo "File path : $filePath"
-# echo "Class Name : $className"
-pkg=$(grep "package" $fileName | awk -F " " '{print $NF}' | cut -d';' -f1)
-# echo "Package : $pkg"
-java -jar myIcfg.jar "$pkg.$className" "$jarFile.dot" "$jarFile"
-# echo java -jar myIcfg.jar "$pkg.$className" "DotFiles/$jarFile.dot" "$jarFile"
-rm -rf $tempFolder
+# echo "java -jar decompiler.jar $jarFile -o $tempFolder"
+java -jar decompiler.jar "$jarFile" -o "$tempFolder" 2> error.txt
+x=$(cat error.txt)
+if [ -z "$x" ]
+then
+    inputJarDir=$tempFolder
+    files=$(grep -inr "applet" "$inputJarDir" | grep -v "abstract" | grep "extends" | head -n1)
+    fileName=$(echo "$files" | cut -d':' -f1)
+    echo "File name : $fileName"
+    # echo "Search results: $files"
+    # filePath=$(echo "$fileName" | rev | cut -d'/' -f2- | rev)
+    className=$(echo "$files" | awk -F 'class' '{print $NF}' )
+    className=$(sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< "$className" | cut -d' ' -f1)
+    # echo "File path : $filePath"
+    # echo "Class Name : $className"
+    pkg=$(grep "package" $fileName | awk -F " " '{print $NF}' | cut -d';' -f1 | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+    echo "Package : $pkg"
+    if [ -z "$pkg" ]
+    then
+        echo java -jar myIcfg.jar "$className" "DotFiles/$jarFile.dot" "$jarFile"
+        java -jar myIcfg.jar "$className" "$jarFile.dot" "$jarFile"
+    else
+        echo java -jar myIcfg.jar "$pkg.$className" "DotFiles/$jarFile.dot" "$jarFile"
+        java -jar myIcfg.jar "$pkg.$className" "$jarFile.dot" "$jarFile"
+    fi
+    rm -rf "$tempFolder"
+else
+    echo "Unable to process $fileName"
+fi
+echo "#############################################################################################################################################"

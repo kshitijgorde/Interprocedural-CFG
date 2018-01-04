26a760f19c6d3a5ba0c09449f1ac09890e15324c
Rahul <rahulr56@users.noreply.github.com>
2018-01-03 19:29:54 -0500

    Update execICFGCreator.sh

--- ICFGCreator/execICFGCreator.sh
+++ ICFGCreator/execICFGCreator.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+
+
+
+
 testerPath="./Tester"
 currentPath=$(pwd)
-files=$(cd $1; ls -l --color=none "$1"/*.jar | awk -F " " '{print $NF}'; cd currentPath)
+files=$(cd $1; ls -l --color=none *.jar | awk -F " " '{print $NF}'; cd currentPath)
 for jarFile in $files
 do
-    # echo "$jarFile"
-	# echo cp "$jarFile" "$testerPath"
-    # echo ./ICFGCreator.sh "$testerPath/$jarFile"
-    # echo rm -rf "$testerPath/$jarFile"
-    # ./ICFGCreator.sh "$testerPath/$jarFile"
-	cp "$jarFile" "$testerPath"
+    echo "$jarFile"
+    echo cp "$jarFile" "$testerPath"
+    echo ./ICFGCreator.sh "$testerPath/$jarFile"
+    echo rm -rf "$testerPath/$jarFile"
+
+    cp "$jarFile" "$testerPath"
     jarFile=$(echo $jarFile | awk -F "/" '{print $NF}')
+    ./ICFGCreator.sh "$testerPath/$jarFile"
     rm -rf $testerPath/$jarFile
 done
